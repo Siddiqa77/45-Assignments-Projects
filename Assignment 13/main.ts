7d00548ea171a27241ac99ba39585462f19c8766
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:45:23 +0500

    Question 13

---
+++ Assignment 13/main.ts
@@ -0,0 +1,6 @@
+// Q no.13 Think of your favorite mode of transportation, such as a motorcycle or a car, and make a 
+//list that stores several examples. Use your list to print 
+//a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+ let transportMode = ['car','Bike','Bus','Sport Car','Taxi'];
+ transportMode.forEach(mode => console.log(`I would like to own a ${mode}.`));
