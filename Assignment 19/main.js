dfff540f15f07a7f67b37f17573e993e4287d8f5
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:49:46 +0500

    Question 19

---
+++ Assignment 19/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q no.18 Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestList = ['Mr.Taha', 'Miss Tanya', 'Mrs.Yasmeen', 'Sir Furqan', 'Hina'];
+// guestList.forEach(oneGuest => console.log(`Assalam-o-Alaikum ${oneGuest}, Would you like to dinner with me?`));
+let lengthGuest = guestList.length;
+console.log(`We are inviting total ${lengthGuest} guests.`);
---
+++ Assignment 19/main.ts
@@ -0,0 +1,10 @@
+// Q no.18 Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+
+let guestList = ['Mr.Taha','Miss Tanya','Mrs.Yasmeen','Sir Furqan','Hina'];
+
+// guestList.forEach(oneGuest => console.log(`Assalam-o-Alaikum ${oneGuest}, Would you like to dinner with me?`));
+
+let lengthGuest: number = guestList.length;
+
+console.log(`We are inviting total ${lengthGuest} guests.`);
