fbf6fea2f178cbdea4c17d821e59047d2efbf98a
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:25:56 +0500

    Question 02

---
+++ Assignment 2/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q no.01 Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = "Eric";
+console.log(`Hello ${personName}, "Would you like to learn some python?"`);
