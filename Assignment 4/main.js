65485e48938c430c4445c1ce1a7c5281d54ed457
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:27:58 +0500

    Question 04

---
+++ Assignment 4/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q no.04 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+Object.defineProperty(exports, "__esModule", { value: true });
+let famousPerson = "Albert Einstein";
+let message = "once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPerson, message);
