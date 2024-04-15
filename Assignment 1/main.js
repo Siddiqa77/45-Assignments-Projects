25e19133c47f0fc4e4a10ba2797391686f213e76
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:23:00 +0500

    Hello World Question 1

---
+++ Assignment 1/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q no.1 Install Node.js, TypeScript and VS Code on your computer.Type Hello world
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Hello World";
+console.log(message);
---
+++ Assignment 1/main.ts
@@ -0,0 +1,6 @@
+// Q no.1 Install Node.js, TypeScript and VS Code on your computer.Type Hello world
+
+let message = "Hello World";
+                        
+console.log(message);
+        
