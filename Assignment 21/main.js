8d8ba07f3a81cd03a445d322f425f98c6e45386b
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:52:07 +0500

    Question 21

---
+++ Assignment 21/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Q no.21 They think of something you could store in a TypeScript Object.
+//  Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let studentName = {
+    name: "Saba",
+    age: 23,
+    marks: 450
+};
+console.log(studentName);
---
+++ Assignment 21/main.ts
@@ -0,0 +1,16 @@
+// Q no.21 They think of something you could store in a TypeScript Object.
+//  Write a program that creates Objects containing these items.
+
+interface studentName {
+    name: string;
+    age: number;
+    marks: number;
+}
+let studentName = { 
+    name : "Saba",
+    age : 23,
+    marks : 450
+};
+
+
+console.log(studentName);
