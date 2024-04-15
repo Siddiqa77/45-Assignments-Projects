9ca61132280952011f04e2e34fcc3314f9e391c2
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:54:05 +0500

    Question 22

---
+++ Assignment 22/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Q no.22 Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error.
+//  Make sure you correct the error before closing the program.
+Object.defineProperty(exports, "__esModule", { value: true });
+//creating a Array
+let errorArray = ['A', 'B', 'C', 'D', 'E'];
+//Producing Error ! by Accessing invalid index of Array
+// console.log(errorArray[7]);
+console.log(errorArray[1]);
---
+++ Assignment 22/main.ts
@@ -0,0 +1,11 @@
+// Q no.22 Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error.
+//  Make sure you correct the error before closing the program.
+
+//creating a Array
+let errorArray: string[] =['A','B','C','D','E'];
+
+//Producing Error ! by Accessing invalid index of Array
+// console.log(errorArray[7]);
+
+//Error removed
+console.log(errorArray[1]);
