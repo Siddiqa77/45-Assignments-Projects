af37fedd5d4cb5fa427a5d1a6b130f97ea28bcc4
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:39:33 +0500

    Question 11

---
+++ Assignment 11/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q no.11 Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+Object.defineProperty(exports, "__esModule", { value: true });
+let friendNames = ['Saba', 'Taha', 'Noor', 'Kiran'];
+friendNames.forEach(listName => console.log(listName));
---
+++ Assignment 11/main.ts
@@ -0,0 +1,8 @@
+// Q no.11 Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+
+let friendNames = ['Saba','Taha','Noor','Kiran'];
+
+friendNames.forEach(listName => console.log(listName));
+
+
