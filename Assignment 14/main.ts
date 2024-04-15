e8e80e37671a4d46ef85a4780c7b989312e394e7
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:45:59 +0500

    Question 14

---
+++ Assignment 14/main.ts
@@ -0,0 +1,7 @@
+// Q no.14 If you could invite anyone, living or deceased, to dinner, who would you invite? Make
+// a list that includes at least three people you’d like to invite to dinner.
+// Then use your list to print a message to each person, inviting them to dinner.
+
+let guestList = ['Mr.Taha','Miss Tanya','Mrs.Yasmeen','Sir Furqan'];
+
+guestList.forEach(oneGuest => console.log(`Assalam-o-Alaikum ${oneGuest}, Would you like to dinner with me?`));
