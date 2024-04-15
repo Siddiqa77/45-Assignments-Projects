80333d4b8481866832e5d640f0bd554bd3a20eb4
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-16 02:53:14 +0500

    Question 37

---
+++ Assignment 37/main.ts
@@ -0,0 +1,17 @@
+// Q no.37 Large Shirts: Modify the make_shirt() function so that shirts are 
+// large by default with a message that reads I love TypeScript. Make a large
+// shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+function make_shirt(size: string = "Large", print_message: string = "I Love TypeScript"){
+    console.log(`Creating a ${size} shirt with ${print_message} prints on shirt`)
+}
+
+//Calling a function by default value
+make_shirt()
+
+// Calling a function with medium size and default message
+
+make_shirt("Medium")
+
+//Calling a function now with small size and different print message
+make_shirt("Small","I Love JavaScript")
