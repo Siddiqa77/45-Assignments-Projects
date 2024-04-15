59f6d48bdcec73c6f9396e30b26f98eb725ea1c4
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:35:31 +0500

    Question 10

---
+++ Assignment 10/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Q no.10 Adding Comments:
+//  Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific 
+//  to write because your programs are too simple at this point, just add your name and the current date at the top of
+//   each program file. Then write one sentence describing what the program does.
+Object.defineProperty(exports, "__esModule", { value: true });
+// In this take Addition as a variable and assign it a Sum calculation
+let Addition = 4 + 4;
+console.log(Addition);
+//In this take Subtraction as a variable and assign it a Minus calculation
+let Subtraction = 10 - 2;
+console.log(Subtraction);
+//In this take Multiplication as a variable and assign it a product calculation
+let Multiplication = 4 * 2;
+console.log(Multiplication);
+//In this take Division as a variable and assign it a Divisioncalculation
+let Division = 16 / 2;
+console.log(Division);
---
+++ Assignment 10/main.ts
@@ -0,0 +1,26 @@
+// Q no.10 Adding Comments:
+//  Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific 
+//  to write because your programs are too simple at this point, just add your name and the current date at the top of
+//   each program file. Then write one sentence describing what the program does.
+
+
+
+// In this take Addition as a variable and assign it a Sum calculation
+let Addition: number = 4 + 4;
+console.log(Addition);
+
+
+//In this take Subtraction as a variable and assign it a Minus calculation
+let Subtraction: number = 10 - 2;
+console.log(Subtraction);
+
+
+//In this take Multiplication as a variable and assign it a product calculation
+let Multiplication: number = 4 * 2;
+console.log(Multiplication);
+
+
+//In this take Division as a variable and assign it a Divisioncalculation
+let Division: number = 16 / 2;
+console.log(Division);
+
