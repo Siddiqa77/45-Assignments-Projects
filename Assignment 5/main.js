f68e0223787eb13dfacb146164b6cc30586ee6fc
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:29:31 +0500

    Question 05

---
+++ Assignment 5/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your
+// message and store it in a new variable called message. Print your message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let person_name = "William Shakespeare";
+let quote = "All that glitters is not gold";
+console.log(`${person_name}, once said, "${quote}" `);
