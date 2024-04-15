d1eed7ab149a28036c0907a681108938c33cb05a
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:51:09 +0500

    Question 20

---
+++ Assignment 20/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Q No.20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+//  Write a program that creates a list containing these items. 
+Object.defineProperty(exports, "__esModule", { value: true });
+//Makin a Cities name Array 
+let citiesName = ['Karachi', 'Lahore', 'Multan', 'Peshawar', 'Islamabad', 'Hyderabad'];
+//print the message of list
+console.log("List of Cities:");
+//print the values of Array in the form of list
+citiesName.forEach(cityName => console.log(cityName));
---
+++ Assignment 20/main.ts
@@ -0,0 +1,13 @@
+
+// Q No.20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+//  Write a program that creates a list containing these items. 
+
+
+//Makin a Cities name Array 
+let citiesName: string[] = ['Karachi','Lahore','Multan','Peshawar','Islamabad','Hyderabad'];
+
+//print the message of list
+console.log("List of Cities:");
+
+//print the values of Array in the form of list
+citiesName.forEach(cityName => console.log(cityName));
