263452bae4f9a493cb79bf44f8ff32bdffe3f9f3
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:33:39 +0500

    Question 09

---
+++ Assignment 9/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//Q no.09 Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number.
+// Print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let favNum = 4;
+let message = `My favourite number is: "${favNum}"`;
+console.log(message);
---
+++ Assignment 9/app.ts
@@ -0,0 +1,12 @@
+//Q no.09 Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number.
+// Print that message.
+
+let favNum: number = 4;
+let message: string = `My favourite number is: "${favNum}"`;
+
+console.log(message);
+ 
+
+
+
+
