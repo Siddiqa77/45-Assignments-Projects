8f81c10c9087970fd38b5e2ca73a4a80b6a7062a
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:44:51 +0500

    Question 12

---
+++ Assignment 12/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Q no. 13 Start with the array you used in Exercise 11,
+// but instead of just printing each person’s name, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+Object.defineProperty(exports, "__esModule", { value: true });
+let friendNames = ['Saba', 'Taha', 'Noor', 'Kiran'];
+friendNames.forEach(listName => console.log(`Hello ${listName}, How are you?`));
---
+++ Assignment 12/main.ts
@@ -0,0 +1,6 @@
+// Q no. 13 Start with the array you used in Exercise 11,
+// but instead of just printing each person’s name, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+
+let friendNames = ['Saba','Taha','Noor','Kiran'];
+friendNames.forEach(listName => console.log(`Hello ${listName}, How are you?`));
