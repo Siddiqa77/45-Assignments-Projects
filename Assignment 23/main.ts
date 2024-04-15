26e7e500c1f844e4f34142fbf80f121006e00576
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:54:31 +0500

    Question 23

---
+++ Assignment 23/main.ts
@@ -0,0 +1,50 @@
+// Q no.23 Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+//Making a variable
+let five = 5;
+let ten = 10;
+// 4 Test of true
+//Test 01
+console.log("Test 01: Five is equal to 5");
+console.log(five == 5);
+
+//Test 02
+console.log("\nTest 02: Ten is equal to 10?");
+console.log(ten == 10);
+
+//Test 03
+console.log("\nTest 03: 5 is not equal to 10?");
+console.log(five != ten);
+
+//Test 04
+console.log("\nTest 04: Ten is greater than five?");
+console.log(10 > 5);
+
+// 4 Test of false
+//Test 05
+console.log("\nTest 05: ten is smaller than five?");
+console.log(10 < 5);
+
+//Test 06
+console.log("\nTest 06: five is greater than ten?");
+console.log(5 > 10);
+
+//Test 07
+console.log("\nTest 07: 5 is equal to 10?");
+console.log(five == ten);
+
+//Test 08
+console.log("\nTest 08: ten is not equal to ten?");
+console.log(10 != 10);
+
+
