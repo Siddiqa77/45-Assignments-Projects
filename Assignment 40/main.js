44afded509df893ab9b25525ae6b9b046664ad33
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-16 04:17:35 +0500

    Question 40

---
+++ Assignment 40/main.js
@@ -0,0 +1,29 @@
+"use strict";
+// Qno.40 Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist 
+// name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries 
+// representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album()
+// that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object.
+//  Make at least one new function call that includes the number of tracks on an album.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Define the make_album function
+function make_album(artistName, albumTitle, trackNumber) {
+    let album = {
+        artist: artistName,
+        title: albumTitle,
+        tracks: trackNumber
+    };
+    if (trackNumber !== undefined) {
+        album.tracks = trackNumber;
+    }
+    return album;
+}
+// calling funtions in three times with different values with 3 variables
+//variable 1
+let album1 = make_album("Atif Aslam", "Album 1", 2);
+//variable 2
+let album2 = make_album("Junaid Jamshed", "Album 4", 4);
+//variable 3
+let album3 = make_album("Hadeeqa", "Album 1", 3);
+console.log(album1);
+console.log(album2);
+console.log(album3);
