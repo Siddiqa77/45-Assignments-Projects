def46ba7571fe82004f339bac9316039d1dffce0
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:32:11 +0500

    Question 06 & 07

---
+++ Assignment 6 & 7/app.js
@@ -0,0 +1,8 @@
+"use strict";
+// Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around 
+//the name is displayed. Then print the name after striping the white spaces.
+Object.defineProperty(exports, "__esModule", { value: true });
+let whiteSpace = 'Siddiqa\tBadar';
+console.log(whiteSpace);
+let whiteSpace2 = 'Siddiqa\nBadar';
+console.log(whiteSpace2);
