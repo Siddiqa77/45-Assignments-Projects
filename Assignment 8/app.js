b7ce83cd3642d85a9b7fcfc04f01baa4e14ec58f
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-15 06:32:58 +0500

    Question 08

---
+++ Assignment 8/app.js
@@ -0,0 +1,14 @@
+"use strict";
+//Q no.07 Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in
+// print statements to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+let Addition = 4 + 4;
+let Subtraction = 10 - 2;
+let Multiplication = 4 * 2;
+let Division = 16 / 2;
+console.log(Addition, Subtraction, Multiplication, Division);
+//Q no.08 You should create four lines that look like this:
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
---
+++ Assignment 8/app.ts
@@ -0,0 +1,17 @@
+//Q no.07 Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in
+// print statements to see the results.
+
+let Addition: number = 4 + 4;
+let Subtraction: number = 10 - 2;
+let Multiplication: number = 4 * 2;
+let Division: number = 16 / 2;
+
+console.log(Addition,Subtraction,Multiplication,Division)
+
+
+//Q no.08 You should create four lines that look like this:
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
